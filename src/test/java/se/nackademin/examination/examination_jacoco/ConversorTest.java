1ff2ce5c8835f8139435c5dddc2e7b0a5e0e9d50
Simone L <lentini.simone@gmail.com>
2021-03-31 14:42:50 +0200

    created the test cases for Conversor method.Tested setters and getters for Name and Gender

--- src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
@@ -5,12 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConversorTest {
-
+	Conversor conversor = new Conversor();
 	@Test
-	public void testConversor() {
-		Conversor conversor = new Conversor();
+	public void testConversorName() {
 		conversor.setNameConverterArrayValues();
 		assertEquals(conversor.getNameConverterArray().get(0), "Your first name is smaller than your last name");
+		assertEquals(conversor.getNameConverterArray().get(1), "Your last name is smaller than your first name");
+		assertEquals(conversor.getNameConverterArray().get(2), "The lengths of your first name is your last name are the same");
 	}
-
+	@Test
+	public void testConversorGender() {
+		conversor.setGenderConverterArrayValues();
+		assertEquals(conversor.getGenderConverterArray().get(0), "man!");
+		assertEquals(conversor.getGenderConverterArray().get(1), "woman!");
+		assertEquals(conversor.getGenderConverterArray().get(2), "person!");
+	}
 }
