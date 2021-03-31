a708238a2ae9e7baef79c127cebbb54007655201
Simone L <lentini.simone@gmail.com>
2021-03-31 19:07:12 +0200

    created test cases for the Game method(calculateOutPutBasedOnNames,calculateOutPutBasedOnGender, calculateOutPutBasedOnAge)

--- src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
@@ -10,19 +10,55 @@ public class GameTest {
 	public void testMethod1() {
 		Game game = new Game();
 		int i = game.calculateOutPutBasedOnNames("Rafael", "Silva");
-		assertEquals( i, 1);
+		assertEquals(i, 1);
 	}
+
 	@Test
 	public void testMethod2() {
 		Game game = new Game();
 		int i = game.calculateOutPutBasedOnNames("Simone", "Lentini");
-		assertEquals( i, 0);
+		assertEquals(i, 0);
 	}
+
 	@Test
 	public void testMethod3() {
 		Game game = new Game();
 		int i = game.calculateOutPutBasedOnNames("Simone", "Lentin");
-		assertEquals( i, 2);
+		assertEquals(i, 2);
 	}
 
+	@Test
+	public void testMethod4() {
+		Game game = new Game();
+		int i = game.calculateOutPutBasedOnGender('M');
+		assertEquals(i, 0);
+	}
+
+	@Test
+	public void testMethod5() {
+		Game game = new Game();
+		int i = game.calculateOutPutBasedOnGender('F');
+		assertEquals(i, 1);
+	}
+
+	@Test
+	public void testMethod6() {
+		Game game = new Game();
+		int i = game.calculateOutPutBasedOnGender('O');
+		assertEquals(i, 2);
+	}
+
+	@Test
+	public void testMethod7() {
+		Game game = new Game();
+		int i = game.calculateOutPutBasedOnAge(25);
+		assertEquals(i, 0);
+	}
+
+	@Test
+	public void testMethod8() {
+		Game game = new Game();
+		int i = game.calculateOutPutBasedOnAge(26);
+		assertEquals(i, 1);
+	}
 }
